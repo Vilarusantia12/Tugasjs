081447074e1b33b7cd3bb7d41cb077bb44ce3886
Vilarusantia12 <129857671+Vilarusantia12@users.noreply.github.com>
2023-04-06 02:27:41 +0700

    Add files via upload

---
+++ tugas/section2.js
@@ -0,0 +1,7 @@
+// section 2 - 4
+// section 2
+// 1
+conts elementsBytag = document.getElementsByTagName("lu");
+for (let i = 0; i < elementsBytag.length; i++) {
+    elementsBytag[i].classList.add("highlight")
+}
